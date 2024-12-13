33c159adcb47b60f05bf734b61a24991f02b1223
trannhon70 <kevintran351996@gmail.com>
2024-12-13 14:13:37 +0700

    update configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'app_mysql',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'nestjs_app',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        database: configService.get('DB_DATABASE'),
+        password: configService.get('DB_PASSWORD'),
+        entities: [],
+        synchronize: configService.get('APP_ENV') === 'development',
+        
+      }),
+      inject: [ConfigService]
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'app_mysql',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'password',
+    //   database: 'nestjs_app',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
